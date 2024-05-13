7692642a03b3aeb73bf721ae70eb9d1b1f1e962f
senda23 <senda.hirotaka.qm@tut.jp>
2024-05-13 19:20:23 +0900

    style: Delete unwanted comments

---
+++ R/sample_func.R
@@ -0,0 +1,10 @@
+#' @title Output sample text
+#' @description \code{sample_func} outputs a sample text
+#'
+#' @export
+#' @examples
+#' # sample_func()
+
+sample_func <- function() {
+  print("Hello, this is a sample function!")
+}
